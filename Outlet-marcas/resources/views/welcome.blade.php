3d5ae209bfd0a83c7aad22d7cedc7e16de53387d
Juliano Rezende <julianomaciel04@gmail.com>
2022-07-12 23:09:36 -0300

    estrutura

---
+++ Outlet-marcas/resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Outlet-Marcas</title>
+</head>
+<body>
+
+    <div class="containerPrincipal">
+
+        <header>
+            <div class="img-banner">
+                <img src="/imagem/logo.jpg" alt="">
+            </div>
+    
+            <div class="nome-marca">
+                <h1>Outlet-Marcas</h1>
+                <p>Transformando sonhos em realidade</p>
+                <button>Saiba mais</button>
+            </div>
+        </header>
+
+    </div>
+    <script src="/javaScript/script.js"></script>
+</body>
+</html>
